26346c2e1101d47f52ec115f4c6abb01073f5cec
Rodriguez, Luis I <luis.i.rodriguez@accenture.com>
2026-02-27 19:13:03 +0100

    DeployFix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/magic-tracker/",
+  base: "/Llliana-Tracker/", // <--- OJO: Debe coincidir exactamente con el nombre del repo
 });
